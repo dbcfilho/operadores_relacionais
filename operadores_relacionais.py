b6d4603adcf46d7bbc620ca6b6b6afdf57fc046b
Douglas Costa <114622378+dbcfilho@users.noreply.github.com>
2023-09-15 14:42:19 -0300

    Add files via upload

---
+++ operadores_relacionais.py
@@ -0,0 +1,26 @@
+a = 5
+b = 10
+
+# Verifica se a é igual a b
+if a == b:
+    print("a é igual a b")
+
+# Verifica se a é diferente de b
+if a != b:
+    print("a é diferente de b")
+
+# Verifica se a é menor que b
+if a < b:
+    print("a é menor que b")
+
+# Verifica se a é maior que b
+if a > b:
+    print("a é maior que b")
+
+# Verifica se a é menor ou igual a b
+if a <= b:
+    print("a é menor ou igual a b")
+
+# Verifica se a é maior ou igual a b
+if a >= b:
+    print("a é maior ou igual a b")
